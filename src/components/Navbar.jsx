566a23e5c65dc6b66615e96f6f3336e55cd0aad8
Abhishek Kumar Sharma <akstenx@gmail.com>
2023-04-21 23:52:41 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.scss'
 // import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive)
+    }
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true
+  }
 
   return (
     <div className={active ? 'navbar active' : 'navbar'}>
@@ -19,8 +37,27 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user">
+              <img src="" alt="" />
+              <span>{currentUser?.username}</span>
+              <div className="options">
+                {
+                  currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )
+                }
+                <span>Orders</span>
+                <span>Messages</span>
+                <span>LogOut</span>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       {active && (
