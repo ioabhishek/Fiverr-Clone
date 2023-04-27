18963b33261d2006a0483392f696edcd5837e9f9
Abhishek Kumar Sharma <akstenx@gmail.com>
2023-04-27 21:43:19 +0530

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Home.scss'
+import Featured from '../../components/featured/Featured'
+import TrustedBy from '../../components/trustedBy/TrustedBy'
+import Slide from "../../components/slide/Slide";
+import CatCard from "../../components/catCard/CatCard";
+// import ProjectCard from "../../components/projectCard/ProjectCard";
+import { cards } from "../../data";
 
 const Home = () => {
   return (
-    <div className='home
-    '>Home</div>
+    <div className='home'>
+      <Featured/>
+      <TrustedBy/>
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
+      
+    </div>
   )
 }
 
