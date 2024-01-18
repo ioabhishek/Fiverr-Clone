bc70ea873e6e4fe9fbec897292fb3b6d6f792881
ioabhishek <65117467+ioabhishek@users.noreply.github.com>
2024-01-18 23:07:10 +0530

    latest changes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
 
-const app = express();
-dotenv.config();
-mongoose.set('strictQuery', true);
+const app = express()
+dotenv.config()
+mongoose.set("strictQuery", true)
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
+    await mongoose.connect(process.env.MONGO)
     console.log("Connected to mongodb")
   } catch (error) {
     console.log(error)
